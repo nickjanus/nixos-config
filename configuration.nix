22e1138cd9b5b1111609448940db2e58f640c5e0
Nick Janus <nickjanus@gmail.com>
2016-06-26 00:35:08 -0400

    Updates config for neovim

--- configuration.nix
+++ configuration.nix
@@ -68,86 +68,88 @@
     };
 
     packageOverrides = pkgs: rec {
-      my_vim = pkgs.vim_configurable.customize {
-        name = "vim";
-        vimrcConfig.customRC = ''
-          set colorcolumn=100 
-          
-          " For vim-colors-solarized
-          syntax enable
-          set background=light " or dark
-          colorscheme solarized
-          
-          """ My settings:
-          " tab with two spaces
-          set nobackup
-          set noswapfile
-          set tabstop=2
-          set shiftwidth=2
-          set softtabstop=2
-          set expandtab
-          set smartcase
-          set autoindent
-          set nostartofline
-          set hlsearch      " highlight search terms
-          set incsearch     " show search matches as you type
-          
-          set mouse=a
-          set cmdheight=2
-          
-          set wildmenu
-          set showcmd
-          
-          set number
-          set ruler
-          set backspace=indent,eol,start " Allows backspace on these character
-          set clipboard=unnamed " Allows copy and paste from iterm2
-          set pastetoggle=<F2> " Press F2 to enter paste mode, for pasting from outside vim
-          
-          " php
-          autocmd Filetype php setlocal tabstop=4 shiftwidth=4 softtabstop=4
-          
-          " ruby
-          autocmd FileType ruby compiler ruby
-          filetype plugin on    " Enable filetype-specific plugins
-          
-          " Those types
-          if has("user_commands")
-            command! -bang -nargs=? -complete=file E e<bang> <args>
-            command! -bang -nargs=? -complete=file W w<bang> <args>
-            command! -bang -nargs=? -complete=file Wq wq<bang> <args>
-            command! -bang -nargs=? -complete=file WQ wq<bang> <args>
-            command! -bang Wa wa<bang>
-            command! -bang WA wa<bang>
-            command! -bang Q q<bang>
-            command! -bang QA qa<bang>
-            command! -bang Qa qa<bang>
-          endif
-        '';
-        vimrcConfig.vam.knownPlugins = pkgs.vimPlugins; # optional
-        vimrcConfig.vam.pluginDictionaries = [
-          # load always
-          { 
-	    # todo: Add the commented out plugins as packages
-            names = [ 
-              "surround"
-              "Solarized"
-              "vim-multiple-cursors"
-              "fugitive"
-              "github:ctrlpvim/ctrlp.vim"
-              # "ack"
-              "Syntastic"
-              # "camelcasemotion"
-              # "numbers"
-              "vim-gitgutter"
-              # "github:elixir-lang/vim-elixir"
-              # "github:tpope/vim-markdown"
-              # "github:vim-ruby/vim-ruby"
-              # "github:ajf/puppet-vim"
-              # "github:elzr/vim-json"
-            ];
-          }
-        ];
+      neovim = pkgs.neovim.override {
+        vimAlias = true;
+	configure = {
+          customRC = ''
+            set colorcolumn=100 
+            
+            " For vim-colors-solarized
+            syntax enable
+            set background=dark " or light
+            colorscheme solarized
+            
+            """ My settings:
+            " tab with two spaces
+            set nobackup
+            set noswapfile
+            set tabstop=2
+            set shiftwidth=2
+            set softtabstop=2
+            set expandtab
+            set smartcase
+            set autoindent
+            set nostartofline
+            set hlsearch      " highlight search terms
+            set incsearch     " show search matches as you type
+            
+            set mouse=a
+            set cmdheight=2
+            
+            set wildmenu
+            set showcmd
+            
+            set number
+            set ruler
+            set backspace=indent,eol,start " Allows backspace on these character
+            set clipboard=unnamed " Allows copy and paste from iterm2
+            set pastetoggle=<F2> " Press F2 to enter paste mode, for pasting from outside vim
+            
+            " php
+            autocmd Filetype php setlocal tabstop=4 shiftwidth=4 softtabstop=4
+            
+            " ruby
+            autocmd FileType ruby compiler ruby
+            filetype plugin on    " Enable filetype-specific plugins
+            
+            " Those types
+            if has("user_commands")
+              command! -bang -nargs=? -complete=file E e<bang> <args>
+              command! -bang -nargs=? -complete=file W w<bang> <args>
+              command! -bang -nargs=? -complete=file Wq wq<bang> <args>
+              command! -bang -nargs=? -complete=file WQ wq<bang> <args>
+              command! -bang Wa wa<bang>
+              command! -bang WA wa<bang>
+              command! -bang Q q<bang>
+              command! -bang QA qa<bang>
+              command! -bang Qa qa<bang>
+            endif
+          '';
+          vam.knownPlugins = pkgs.vimPlugins; # optional
+          vam.pluginDictionaries = [
+            # load always
+            { 
+	      # todo: Add the commented out plugins as packages
+              names = [ 
+                "surround"
+                "Solarized"
+                "vim-multiple-cursors"
+                "fugitive"
+                # "github:ctrlpvim/ctrlp.vim"
+                # "ack"
+                "Syntastic"
+                # "camelcasemotion"
+                # "numbers"
+                "vim-gitgutter"
+                # "github:elixir-lang/vim-elixir"
+                # "github:tpope/vim-markdown"
+                # "github:vim-ruby/vim-ruby"
+                # "github:ajf/puppet-vim"
+                # "github:elzr/vim-json"
+              ];
+            }
+          ];
+        };
       };
     };
   };
