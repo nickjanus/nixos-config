dc4917f0e97fbd0e424a1770ec0d5ffc9ca05d30
Nick Janus <nickjanus@gmail.com>
2016-08-17 03:56:42 -0400

    Continues factoring out machine specific details from configuration.nix

---
+++ machines/hydra.nix
@@ -0,0 +1,31 @@
+# This file is symlinked to ../machine.nix on my desktop
+
+{ lib, pkgs, default_services }:
+
+{
+  # Use the gummiboot efi boot loader.
+  boot.loader.gummiboot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.initrd.luks.devices = [
+    {
+      name = "nixos";
+      device = "/dev/VolGroup/nixos";
+      preLVM = false;
+    }
+  ];
+
+  networking.hostName = "nicknix";
+
+  services = lib.recursiveUpdate default_services {
+    xserver = {
+      videoDrivers = [ "nvidia" ];
+      displayManager.slim = {
+        theme = pkgs.fetchurl {
+          url    = "https://github.com/nickjanus/nixos-slim-theme/archive/2.1.tar.gz";
+          sha256 = "8b587bd6a3621b0f0bc2d653be4e2c1947ac2d64443935af32384bf1312841d7";
+        };
+      };
+    };
+  };
+}
