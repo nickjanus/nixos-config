e7ba4fb53c60b54cf26d373de28f6ca3a08fc0ee
Nick Janus <nickjanus@gmail.com>
2016-08-26 00:24:24 -0400

    Enable some zsh modules

--- zsh.nix
+++ zsh.nix
@@ -20,6 +20,8 @@ let
         'completion' \
         'prompt' \
         'fasd' \
+        'ssh' \
+        'screen' \
         'nix'
       # Set the key mapping style to 'emacs' or 'vi'.
       zstyle ':prezto:module:editor' key-bindings 'vi'
