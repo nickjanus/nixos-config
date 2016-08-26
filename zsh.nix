44ba080cfadee1432268b006778ed324d458f792
Nick Janus <nickjanus@gmail.com>
2016-08-26 00:28:43 -0400

    Remove indicator function for normal mode

--- zsh.nix
+++ zsh.nix
@@ -64,13 +64,6 @@ let
 
       # -------------------------------------------------
 
-      function zle-line-init zle-keymap-select {
-        RPS1="''${''${KEYMAP/vicmd/-- NORMAL --}/(main|viins)/-- INSERT --}"
-        RPS2=$RPS1
-        zle reset-prompt
-      }
-      zle -N zle-line-init
-      zle -N zle-keymap-select
       export KEYTIMEOUT=1
     '';
 in {
