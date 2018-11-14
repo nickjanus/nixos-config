d46c73819ac397f1841579bfbd3dc01eebd6102a
nickjanus <nickjanus@gmail.com>
2018-11-14 11:04:18 -0500

    Makes vim a little easier to read

--- vim.nix
+++ vim.nix
@@ -27,6 +27,7 @@ pkgs.neovim.override {
       set showcmd
 
       set number
+      set cursorline
       set ruler
       set backspace=indent,eol,start " Allows backspace on these character
       set clipboard=unnamedplus
