64cc4ce36a95b529d5afb9c63d66b59fbe947835
nickjanus <nickjanus@gmail.com>
2023-04-25 22:13:35 -0400

    installs exa and alias

--- configuration.nix
+++ configuration.nix
@@ -31,6 +31,7 @@ let
     direnv
     efibootmgr
     efivar
+    exa
     file
     firefox
     fwupd
@@ -163,10 +164,7 @@ in {
         ergodox-update = "sudo teensy-loader-cli --mcu=atmega32u4 -v -w";
         grab = "grim -g (slurp)";
         xprimary = "xrandr --output (xrandr --listactivemonitors | grep 2560 | cut -f 6 -d ' ') --primary";
-
-        # Work
-        vpnup = "sudo openconnect --background --protocol=gp -b -u njanus --csd-wrapper ${pkgs.openconnect}/libexec/openconnect/hipreport.sh https://vpn-nyc3.digitalocean.com/ssl-vpn";
-        vpndown = "sudo kill -s INT (pgrep openconnect)";
+        ls = "exa --group-directories-first --color=auto";
       };
     };
     sway = {
